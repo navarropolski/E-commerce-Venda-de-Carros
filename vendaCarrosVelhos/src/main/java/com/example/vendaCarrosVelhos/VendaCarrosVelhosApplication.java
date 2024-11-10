51e51d3cb4b860a51cba179f06eaaf2e5b3b54f2
Lieslei Adriano Navarro <lieslei.navarro@gmail.com>
2024-11-10 18:13:12 -0300

    chore: criação do spring
    inicio do projeto, criacao do spring

---
+++ vendaCarrosVelhos/src/main/java/com/example/vendaCarrosVelhos/VendaCarrosVelhosApplication.java
@@ -0,0 +1,13 @@
+package com.example.vendaCarrosVelhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaCarrosVelhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaCarrosVelhosApplication.class, args);
+	}
+
+}
