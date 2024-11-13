df11ac669ddc6a2e341020049860082e999432ab
Lieslei Adriano Navarro <lieslei.navarro@gmail.com>
2024-11-13 08:59:05 -0300

    chore: controller, dependencies, endpoints
    Criado e mapeado classe controller e criado endpoint get pra ela e adicionado dependencias jpa e mysql no pom.

---
+++ vendaCarrosVelhos/src/main/java/com/example/vendaCarrosVelhos/controller/CarController.java
@@ -0,0 +1,15 @@
+package com.example.vendaCarrosVelhos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("car")
+public class CarController {
+
+    @GetMapping
+    public void getAll(){
+
+    }
+}
